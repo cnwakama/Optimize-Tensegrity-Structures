e5b9e078bfa5835961869606517211850a3f7374
Chibuzo Nwakama <chibuzo-nwakama@uiowa.edu>
2018-05-05 10:00:44 -0500

    create test.py to test the code

--- Functions.py
+++ Functions.py
@@ -12,7 +12,7 @@ class Functions:
 
 
 
-    def __init__(self, lengths, xss, xee, masses):
+    def __init__(self, l, xss, xee, tubeLength):
         #self.x = symbols('x')
         self.xs = sy.symbols('xs')  # starting point of the tube
         self.xe = sy.symbols('xe')  # ending point of the tube
@@ -22,12 +22,12 @@ class Functions:
         self.e33 = np.array([0,0,1])
         #self.e3 = Matrix([0, 0, 1])
         self.gravity = 9.82
-        self.length = sy.symbols('l')
-        self.mass = sy.symbols('m')
-        self.lengths = lengths
+        self.l = sy.symbols('l')
+        self.m = sy.symbols('m')
+        self.length = l
         self.xss = xss
         self.xee = xee
-        self.masses = masses
+        self.mass = 0.28 * tubeLength
 
 
     #x = symbols('x');
@@ -36,18 +36,20 @@ class Functions:
         self.xee = xe
 
     def function(self, f):
-        func = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], f, "numpy")
+        func = sy.lambdify([self.xe, self.xs, self.l, self.m, self.e3], f, "numpy")
         #g = [sy.diff(f, x) for x in [self.xs, self.xs]]
         g = derive_by_array(f, [self.xs, self.xe])
-        grad = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], g, "numpy")
+        grad = sy.lambdify([self.xe, self.xs, self.l, self.m, self.e3], g, "numpy")
         h = derive_by_array(g, [self.xs, self.xe])
-        hess = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], h, "numpy")
+        hess = sy.lambdify([self.xe, self.xs, self.l, self.m, self.e3], h, "numpy")
 
-        #f1 = func(self.xss,self.xee,self.lengths, self.masses)
-        #g1 = grad(self.xss,self.xee,self.lengths, self.masses)
-        #h1 = hess(self.xss,self.xee,self.lengths, self.masses)
+        f1 = np.sum(func(self.xss,self.xee,self.length, self.mass, self.e33))
+        g1 = np.sum(grad(self.xss,self.xee,self.length, self.mass, self.e33),1)
+        h1 = hess(self.xss,self.xee,self.length, self.mass, self.e33)
 
-        return (func, g, h)
+        print g
+
+        return (f1, g1, h1)
     #compute the energy function E (x), its gradient, and its Hessian matrix for any given x
     #@vectorize(0, 1)
     def totalEnergy(self,f1, f2):
@@ -64,7 +66,7 @@ class Functions:
     #m is an array
     #sy.transpose(self.e3)
     def gravitationalEnergy(self):
-        f = self.mass * self.gravity * (1.0/2.0) * self.e3 * (self.xs + self.xe)
+        f = self.mass * self.gravity * (1.0/2.0) * np.dot( self.e3, (self.xs + self.xe))
         #g = [sy.diff(f, x) for x in [self.xs, self.xs]]
             #m * g (1.0/2.0)  * transpose(e)
         #h = sy.hessian(f, [self.xs, self.xe])# should be 0;
@@ -81,19 +83,26 @@ class Functions:
 #, [2, 2, 4],[2, 2, 4]
 #, [4, 5, 6],[1, 20, 5]
 #, 40,40
-f = Functions([2, 2, 4], [[2, 2, 4]], [[1, 1, 2]], [40])
+#, [2, 2, 4],[2, 2, 4], [1, 1, 1], [2, 2, 4],[2, 2, 4]
+#, [2, 2, 4],[2, 2, 4], [1, 1, 1], [2, 2, 4],[2, 2, 4]
+#,2,2,2,2,2
+s = np.array([[1, 1, 1]], dtype=float)
+e = np.array([[1, 1, 1]], dtype=float)
+m = np.array(2, dtype=float)
+l = np.array(1, dtype=float)
+#e3 = np.array([[0, 0, 1],[0, 0, 1],[0, 0, 1], [0, 0, 1],[0, 0, 1],[0, 0, 1]], dtype=float)
+f = Functions(l,s ,e ,m )
 
 [f1, g1, h1] = f.function(f.gravitationalEnergy())
 print f.gravitationalEnergy()
-s = np.array([[2] ,[2], [4]], dtype=float)
-e = np.array([[1], [1], [2]], dtype=float)
-m = np.array([40], dtype=float)
-l = np.array([2], dtype=float)
-print f1(s,e,l,m)
-m = 0
+
+
+#print np.matmul(s,e3)
+print f1
 print g1
-print g1.shape
 print h1
+#print g1.shape
+#print h1
 #[m ,n] = np.size(h1)
 #print m
 #print n
