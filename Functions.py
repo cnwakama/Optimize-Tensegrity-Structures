9d9d6ef8ea7a52951f389b6ac76d2bf63a832e39
Chibuzo Nwakama <chibuzo-nwakama@uiowa.edu>
2018-05-04 23:08:40 -0500

    create test.py to test the code

--- Functions.py
+++ Functions.py
@@ -18,7 +18,9 @@ class Functions:
         self.xe = sy.symbols('xe')  # ending point of the tube
         #self.f = sy.Function('f')
         self.Eprime = 2.5 * 10 ** 6
-        self.e3 = Matrix([0, 0, 1])
+        self.e3 = sy.symbols('e3')
+        self.e33 = np.array([0,0,1])
+        #self.e3 = Matrix([0, 0, 1])
         self.gravity = 9.82
         self.length = sy.symbols('l')
         self.mass = sy.symbols('m')
@@ -34,12 +36,12 @@ class Functions:
         self.xee = xe
 
     def function(self, f):
-        func = sy.lambdify([self.xe, self.xs, self.length, self.mass], f, "numpy")
+        func = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], f, "numpy")
         #g = [sy.diff(f, x) for x in [self.xs, self.xs]]
         g = derive_by_array(f, [self.xs, self.xe])
-        grad = sy.lambdify([self.xe, self.xs, self.length, self.mass], g, "numpy")
+        grad = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], g, "numpy")
         h = derive_by_array(g, [self.xs, self.xe])
-        hess = sy.lambdify([self.xe, self.xs, self.length, self.mass], h, "numpy")
+        hess = sy.lambdify([self.xe, self.xs, self.length, self.mass, self.e3], h, "numpy")
 
         #f1 = func(self.xss,self.xee,self.lengths, self.masses)
         #g1 = grad(self.xss,self.xee,self.lengths, self.masses)
@@ -60,8 +62,9 @@ class Functions:
         #totalEnergy = m*g * (1/2) * math.e * (x1 + x2) + (1/2) * E * (max(0, abs(x1 - x2) - l))
 
     #m is an array
+    #sy.transpose(self.e3)
     def gravitationalEnergy(self):
-        f = self.mass * self.gravity * (1.0/2.0) * sy.transpose(self.e3) * (self.xs + self.xe)
+        f = self.mass * self.gravity * (1.0/2.0) * self.e3 * (self.xs + self.xe)
         #g = [sy.diff(f, x) for x in [self.xs, self.xs]]
             #m * g (1.0/2.0)  * transpose(e)
         #h = sy.hessian(f, [self.xs, self.xe])# should be 0;
@@ -82,8 +85,8 @@ f = Functions([2, 2, 4], [[2, 2, 4]], [[1, 1, 2]], [40])
 
 [f1, g1, h1] = f.function(f.gravitationalEnergy())
 print f.gravitationalEnergy()
-s = np.array([[2, 2, 4]], dtype=float)
-e = np.array([[1, 1, 2]], dtype=float)
+s = np.array([[2] ,[2], [4]], dtype=float)
+e = np.array([[1], [1], [2]], dtype=float)
 m = np.array([40], dtype=float)
 l = np.array([2], dtype=float)
 print f1(s,e,l,m)
---
+++ test.py
@@ -0,0 +1,11 @@
+import TrustRegion
+import Functions
+
+f = 0
+g = 0
+h = 0
+c = 0
+gc= 0 
+m = 0
+mu = 0
+trustRegion = TrustRegion(f, g, h, c, gc, m, mu)
